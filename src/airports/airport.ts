7a0fc4a9c46b1d26ff80b9445a1abfc1f8cc100c
hnamzian <namazian87@gmail.com>
2022-02-09 16:24:37 +0300

    Create Airport service

---
+++ src/airports/airport.ts
@@ -0,0 +1,9 @@
+export class Airport {
+  public name: string
+  public location: string
+
+  constructor(name: string, location: string) {
+    this.name = name
+    this.location = location
+  }
+}
---
+++ src/airports/airports.service.ts
@@ -0,0 +1,36 @@
+import { Context } from "fabric-contract-api";
+import { Airport } from "./airport";
+
+// ToDo Implement and test update && delete methods
+export class AirtposService {
+    private ctx: Context;
+
+    constructor(ctx) {
+        this.ctx = ctx;
+    }
+
+    async exists(airportId: string) {
+        const airportBuffer = await this.ctx.stub.getState(airportId);
+        if (airportBuffer && airportBuffer.length > 0) return true;
+        return false;
+    }
+
+    async get(airportId: string) {
+        const airportBuffer = await this.ctx.stub.getState(airportId)
+        return JSON.parse(airportBuffer.toString())
+    }
+
+    async create(airportId: string, name: string, location: string) {
+        if (await this.exists(airportId)) {
+            throw new Error(`Airport ${airportId} already exists`);
+        }
+
+        let airport = new Airport(name, location);
+
+        // db.pudState(airlineId, airline)
+
+        const airportBuffer = Buffer.from(JSON.stringify(airport));
+
+        await this.ctx.stub.putState(airportId, airportBuffer);
+    }
+}
